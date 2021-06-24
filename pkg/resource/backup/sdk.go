6cfd3f7c046c0ed02054532bb64b463ce9e4471c
Amine <hilalyamine@gmail.com>
2021-06-24 18:06:41 +0200

    Bump runtime version and regenerates the controller with `ack-generate` v0.3.1 (#7)
    Regenerates the DynamoDB controller with `ack-generate` v0.3.1 
    to bring in runtime `v0.3.0` improvements and features.
    
    By submitting this pull request, I confirm that my contribution is made under
    the terms of the Apache 2.0 license.

--- pkg/resource/backup/sdk.go
+++ pkg/resource/backup/sdk.go
@@ -29,7 +29,6 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	svcapitypes "github.com/aws-controllers-k8s/dynamodb-controller/apis/v1alpha1"
-	svcsdkapi "github.com/aws/aws-sdk-go/service/dynamodb"
 )
 
 // Hack to avoid import errors during build...
@@ -41,7 +40,6 @@ var (
 	_ = &svcapitypes.Backup{}
 	_ = ackv1alpha1.AWSAccountID("")
 	_ = &ackerr.NotFound
-	_ = svcsdkapi.New
 )
 
 // sdkFind returns SDK-specific information about a supplied resource
@@ -64,7 +62,7 @@ func (rm *resourceManager) sdkFind(
 		return nil, err
 	}
 
-	var resp *svcsdkapi.DescribeBackupOutput
+	var resp *svcsdk.DescribeBackupOutput
 	resp, err = rm.sdkapi.DescribeBackupWithContext(ctx, input)
 	rm.metrics.RecordAPICall("READ_ONE", "DescribeBackup", err)
 	if err != nil {
@@ -164,7 +162,8 @@ func (rm *resourceManager) sdkCreate(
 		return nil, err
 	}
 
-	var resp *svcsdkapi.CreateBackupOutput
+	var resp *svcsdk.CreateBackupOutput
+	_ = resp
 	resp, err = rm.sdkapi.CreateBackupWithContext(ctx, input)
 	rm.metrics.RecordAPICall("CREATE", "CreateBackup", err)
 	if err != nil {
@@ -293,6 +292,7 @@ func (rm *resourceManager) setStatusDefaults(
 // else it returns nil, false
 func (rm *resourceManager) updateConditions(
 	r *resource,
+	onSuccess bool,
 	err error,
 ) (*resource, bool) {
 	ko := r.ko.DeepCopy()
@@ -301,12 +301,16 @@ func (rm *resourceManager) updateConditions(
 	// Terminal condition
 	var terminalCondition *ackv1alpha1.Condition = nil
 	var recoverableCondition *ackv1alpha1.Condition = nil
+	var syncCondition *ackv1alpha1.Condition = nil
 	for _, condition := range ko.Status.Conditions {
 		if condition.Type == ackv1alpha1.ConditionTypeTerminal {
 			terminalCondition = condition
 		}
 		if condition.Type == ackv1alpha1.ConditionTypeRecoverable {
 			recoverableCondition = condition
+		}
+		if condition.Type == ackv1alpha1.ConditionTypeResourceSynced {
+			syncCondition = condition
 		}
 	}
 
@@ -348,7 +352,9 @@ func (rm *resourceManager) updateConditions(
 			recoverableCondition.Message = nil
 		}
 	}
-	if terminalCondition != nil || recoverableCondition != nil {
+	// Required to avoid the "declared but not used" error in the default case
+	_ = syncCondition
+	if terminalCondition != nil || recoverableCondition != nil || syncCondition != nil {
 		return &resource{ko}, true // updated
 	}
 	return nil, false // not updated
