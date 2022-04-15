f787aff2243b5945df9fc47b756e5beb5559b45b
ci-robot <ci-robot@k8s.io>
2022-04-15 17:38:01 +0000

    Update to ACK runtime `v0.18.4`, code-generator `v0.18.4`

--- cmd/controller/main.go
+++ cmd/controller/main.go
@@ -18,6 +18,7 @@ package main
 import (
 	"os"
 
+	ackv1alpha1 "github.com/aws-controllers-k8s/runtime/apis/core/v1alpha1"
 	ackcfg "github.com/aws-controllers-k8s/runtime/pkg/config"
 	ackrt "github.com/aws-controllers-k8s/runtime/pkg/runtime"
 	ackrtutil "github.com/aws-controllers-k8s/runtime/pkg/util"
@@ -31,11 +32,12 @@ import (
 
 	svctypes "github.com/aws-controllers-k8s/dynamodb-controller/apis/v1alpha1"
 	svcresource "github.com/aws-controllers-k8s/dynamodb-controller/pkg/resource"
-	ackv1alpha1 "github.com/aws-controllers-k8s/runtime/apis/core/v1alpha1"
 
 	_ "github.com/aws-controllers-k8s/dynamodb-controller/pkg/resource/backup"
 	_ "github.com/aws-controllers-k8s/dynamodb-controller/pkg/resource/global_table"
 	_ "github.com/aws-controllers-k8s/dynamodb-controller/pkg/resource/table"
+
+	"github.com/aws-controllers-k8s/dynamodb-controller/pkg/version"
 )
 
 var (
@@ -101,7 +103,11 @@ func main() {
 	)
 	sc := ackrt.NewServiceController(
 		awsServiceAlias, awsServiceAPIGroup, awsServiceEndpointsID,
-		ackrt.VersionInfo{}, // TODO: populate version info
+		ackrt.VersionInfo{
+			version.GitCommit,
+			version.GitVersion,
+			version.BuildDate,
+		},
 	).WithLogger(
 		ctrlrt.Log,
 	).WithResourceManagerFactories(
