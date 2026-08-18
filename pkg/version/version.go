59c3146a6f73f5dd8029682278d2505ba3c1f089
Daníel P. Purkhús <purkhusid@gmail.com>
2026-08-18 18:33:39 +0000

    Regenerate

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -28,5 +28,5 @@ var (
 // time and are immutable for the life of the generated code.
 const (
 	ACKGenerateVersion   = "v0.62.1"
-	ACKGenerateBuildDate = "2026-08-13T00:57:50Z"
+	ACKGenerateBuildDate = "2026-08-18T18:24:04Z"
 )
